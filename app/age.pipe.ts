11c42de45ada7106fcf5f4c7a99918477f429703
Abdullah Yahya <33635135+AbdulYahya@users.noreply.github.com>
2018-02-28 08:44:31 -0800

    Update age.pipe.ts

--- app/age.pipe.ts
+++ app/age.pipe.ts
@@ -15,14 +15,12 @@ export class AgePipe implements PipeTransform {
         this.output = input.filter((animal, array) => {
           return (animal.age < 2) ? animal : null;
         });
-        console.log(this.output);
         return this.output;
 
       case 'matureFilter':
         this.output = input.filter((animal, array) => {
           return (animal.age >= 2) ? animal : null;
         });
-
         return this.output;
 
       default:
